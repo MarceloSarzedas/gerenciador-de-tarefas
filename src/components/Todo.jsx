bd3bcf10d7bce2ebf5225dbb29f75aa0ba9cc752
marcelo <marcelohdjusto@hotmail.com>
2024-08-15 13:10:00 -0300

    ok
    Foi adicionando botão de editar esta sem funcionamento, também foi trocado os botoes que está escreto pelos ícones da biblioteca 'React-icone'

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,24 +1,31 @@
-import PropTypes from 'prop-types';// sem fucionar
 
-const Todo = ({ todo, removerTodo, completeTodo}) => {
+import PropTypes from 'prop-types';// sem fucionar
+import { VscChromeClose, VscCheck } from "react-icons/vsc";
+/*  import { VscEdit } from "react-icons/vsc";  */ 
+const Todo = ({ todo, removerTodo, completeTodo, /* editarTodo */ }) => {
+  
   return (
     <div
-     className="todo" style={{ textDecoration: todo.completed ? "line-through" : "" }}
+      className="todo" style={{ textDecoration: todo.completed ? "line-through" : "" }}
     >
-          <div className='content'>
-            <p>{todo.text}</p>
-            <p className="category">{todo.category}</p>
-          </div>
-          <div>
-          <button className="completo" 
-          onClick={()=> completeTodo(todo.id)}>
-            completa</button>
-          
-          <button className="remover" 
-          onClick={()=> removerTodo(todo.id)}>
-          excluir</button>
-          </div>
-        </div>
+      <div className='content'>
+        <p>{todo.text}</p>
+        <p className="category">{todo.category}</p>
+      </div>
+      <div>
+       {/*  <button className="editar" 
+        onClick={() => editarTodo(todo.id)}>
+          <VscEdit /></button>  */}
+
+        <button className="completo"
+          onClick={() => completeTodo(todo.id)}>
+          <VscCheck /></button>
+
+        <button className="remover"
+          onClick={() => removerTodo(todo.id)}>
+          <VscChromeClose /></button>
+      </div>
+    </div>
   )
 }
 
@@ -26,5 +33,6 @@ Todo.propTypes = {
   todo: PropTypes.object.isRequired,
   removerTodo: PropTypes.func.isRequired,
   completeTodo: PropTypes.func.isRequired,
+  /* editarTodo: PropTypes.func.isRequired, */
 };// sem fucionar
 export default Todo
